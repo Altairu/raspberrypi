cdab1772e79447607fb8099a025ec145d212c981
-Altair- <115722320+Altairu@users.noreply.github.com>
2023-10-18 03:49:06 +0900

    Update zikoichi.py

--- python/zikoichi.py
+++ python/zikoichi.py
@@ -13,11 +13,16 @@ y_positions = []
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
 # フィールドの寸法
-field_width = 7000  # フィールドの幅 (mm)
-field_height = 3462  # フィールドの高さ (mm)
+field_width = 3462  # フィールドの幅 (mm)
+field_height = 7000  # フィールドの高さ (mm)
+
+# フィールド写真のサイズ
+img_width = 637  # フィールド写真の幅 (ピクセル)
+img_height = 1267  # フィールド写真の高さ (ピクセル)
 
 # 画像の表示
-plt.imshow(field_img, extent=[0, field_height, 0, field_width])
+plt.figure(figsize=(img_width / 100, img_height / 100))  # グラフのサイズを指定 (ピクセルをcmに変換)
+plt.imshow(field_img, extent=[0, field_width, 0, field_height])
 
 while True:
     data = ser.readline().decode().strip()
@@ -26,8 +31,8 @@ while True:
         y, x, _ = map(int, data.split(", "))  # y を縦軸、x を横軸として受信
 
         # ロボットの座標データをフィールドの寸法に合わせて変換
-        x = x * field_width / 637
-        y = y * field_height / 1267
+        x = x * field_width / img_width
+        y = y * field_height / img_height
 
         x_positions.append(x)
         y_positions.append(y)
