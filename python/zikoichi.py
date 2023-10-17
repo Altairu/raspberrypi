8f29a792ece11813aabe3a32be3dcba0bc697b37
-Altair- <115722320+Altairu@users.noreply.github.com>
2023-10-18 03:59:49 +0900

    Update zikoichi.py

--- python/zikoichi.py
+++ python/zikoichi.py
@@ -6,12 +6,8 @@ import numpy as np
 # フィールド写真の読み込み
 field_img = mpimg.imread('field.png')
 
-# プロット用のリスト
-x_positions = []
-y_positions = []
-
 # シリアル通信の設定
-ser = serial.Serial('/dev/ttyACM3', 115200)
+ser = serial.Serial('/dev/ttyACM4', 115200)
 
 # フィールドの寸法
 field_width = 3462  # フィールドの幅 (mm)
@@ -25,6 +21,10 @@ img_height = 1267  # フィールド写真の高さ (ピクセル)
 plt.figure(figsize=(img_width / 100, img_height / 100))  # グラフのサイズを指定 (ピクセルをcmに変換)
 plt.imshow(field_img, extent=[0, field_width, 0, field_height])
 
+arrow = plt.arrow(0, 0, 0, 0, head_width=50, head_length=100, fc='g', ec='g')
+plt.xlim(0, field_width)
+plt.ylim(0, field_height)
+
 while True:
     data = ser.readline().decode().strip()
     if data.startswith("(") and data.endswith(")"):
@@ -35,21 +35,12 @@ while True:
         x = x * field_width / img_width
         y = y * field_height / img_height
 
-        x_positions.append(x)
-        y_positions.append(y)
+        # ロボットの向きを示す矢印を更新
+        arrow.remove()
+        arrow = plt.arrow(x, y, 100 * np.cos(np.deg2rad(theta)), 100 * np.sin(np.deg2rad(theta)),
+                          head_width=50, head_length=100, fc='g', ec='g')
 
-        # プロットの更新
-        plt.scatter(y_positions, x_positions, c='r', marker='o')
         plt.pause(0.01)
-
-    # ロボットの向きを示す線を描画
-    if len(x_positions) > 1:
-        current_x, current_y = x_positions[-1], y_positions[-1]
-        line_length = 100  # ロボットの向きを示す線の長さ
-        end_x = current_x + line_length * np.cos(np.deg2rad(theta))
-        end_y = current_y + line_length * np.sin(np.deg2rad(theta))
-        plt.plot([current_x, end_x], [current_y, end_y], 'g')
-
 
 # グラフを閉じる
 plt.close()
